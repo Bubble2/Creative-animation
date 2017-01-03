10c48ea9be516fc6db41714fc6eb373967a83c2d
Bubble2 <guozhaodong318@163.com>
2017-01-03 16:21:51 +0800

    add new animation

--- js/demo.js
+++ js/demo.js
@@ -13,40 +13,6 @@
 			oIcon.classList.toggle('icon-menu-on');
 			oIcon.classList.add('tap-feedback-anim');
 		});
-
-
-	}
-
-	
-	function autoPlay(){
-		for(var j=0;j<aBtnMenu.length;j++){
-			aBtnMenu[j].count=0;
-			var oIcon=aBtnMenu[j].children[0];
-			oIcon.classList.remove('icon-menu-on');
-			oIcon.classList.remove('icon-menu-transition');
-			oIcon.classList.add('icon-menu-anim');
-			oIcon.classList.add('tap-feedback-anim');
-			oIcon.style.transform='none';
-		}
-	}
-
-	function stopPlay(){
-		for(var j=0;j<aBtnMenu.length;j++){
-			var oIcon=aBtnMenu[j].children[0];
-			oIcon.classList.add('icon-menu-transition');
-			oIcon.classList.remove('icon-menu-anim');
-			oIcon.classList.remove('tap-feedback-anim');
-		}
-	}
-	
-	autoPlay();
-
-	var oDemoBox1=document.querySelector('.demo-box1');
-	oDemoBox1.onmouseenter=function(){
-		stopPlay();
-	}
-	oDemoBox1.onmouseleave=function(){
-		autoPlay();
 	}
 })();
 
